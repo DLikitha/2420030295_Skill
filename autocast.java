56aa23823e4a4c401eebe836626eae76fe748b06
DLikitha <2420030295@klh.edu.in>
2025-08-05 10:18:34 +0530

    Add files via upload

---
+++ autocast.java
@@ -0,0 +1,10 @@
+package myproject;
+
+public class autocast {
+       public static void main(String[]args) {
+    	   int myint= 9;
+    	   double mydouble= myint;
+    	   System.out.println(myint);
+    	   System.out.println(mydouble);
+       }
+}
---
+++ manualcast.java
@@ -0,0 +1,11 @@
+package myproject;
+
+public class manualcast {
+     public static void main(String[]args) {
+    	 double mydouble= 9.78;
+    	 int myint = (int)mydouble;
+    	 //manual casting 
+    	 System.out.println(mydouble);
+    	 System.out.println(myint);
+     }
+}
